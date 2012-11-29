c86ef0d9c70797552f11b70483c39ece1de83fe6
John Davalos <johnd@jiff.com>
2012-11-28 20:54:52 -0800

    debugging user session

--- app/controllers/dj_mon/application_controller.rb
+++ app/controllers/dj_mon/application_controller.rb
@@ -1,2 +1,9 @@
 class DjMon::ApplicationController < ApplicationController
+  
+  private 
+  def authenticate_djmon_user
+    if !djmon_user
+      logger.debug "~~~~~~~~NO USER"
+    end
+  end
 end
