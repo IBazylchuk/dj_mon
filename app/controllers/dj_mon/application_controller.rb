8fcf0d85d97784773df3d13e59d2ecaa2e584ae7
John Davalos <johnd@jiff.com>
2012-12-03 09:28:18 -0800

    adding some logging for debugging

--- app/controllers/dj_mon/application_controller.rb
+++ app/controllers/dj_mon/application_controller.rb
@@ -2,6 +2,7 @@ class DjMon::ApplicationController < ApplicationController
   
   private 
   def authenticate_djmon_user
+  	logger.debug "~~~~~~~~~~~~HELLO FROM DJ MON!!!"
     if !djmon_user
       logger.debug "~~~~~~~~NO USER"
     end
