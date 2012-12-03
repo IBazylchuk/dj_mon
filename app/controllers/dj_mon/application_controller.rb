62089d775aed08b30a9f2ede3ffea602731c5ed0
John Davalos <johnd@jiff.com>
2012-12-03 09:34:31 -0800

    updating application controller

--- app/controllers/dj_mon/application_controller.rb
+++ app/controllers/dj_mon/application_controller.rb
@@ -1,5 +1,5 @@
 class DjMon::ApplicationController < ApplicationController
-  
+  helper_method :authenticate_djmon_user
   private 
   def authenticate_djmon_user
   	logger.debug "~~~~~~~~~~~~HELLO FROM DJ MON!!!"
